45cff93ae980f58b1b467d879fc6342a03ec9f7f
JoelJonas <joel.deoliveira@salsatechonology.com>
2023-08-26 14:03:13 -0300

    TDD Test Green/Refactor update produto

--- src/main/java/br/com/bernhoeft/service/impl/ProdutoServiceImpl.java
+++ src/main/java/br/com/bernhoeft/service/impl/ProdutoServiceImpl.java
@@ -1,11 +1,14 @@
 package br.com.bernhoeft.service.impl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.bernhoeft.dto.ProdutoDTO;
 import br.com.bernhoeft.model.Produto;
 import br.com.bernhoeft.repository.ProdutoRepository;
 import br.com.bernhoeft.service.ProdutoService;
+import jakarta.persistence.EntityNotFoundException;
 
 public class ProdutoServiceImpl implements ProdutoService {
 	
@@ -19,8 +22,18 @@ public class ProdutoServiceImpl implements ProdutoService {
 
 	@Override
 	public Produto update(ProdutoDTO produtoDTO) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Produto> produtoOptional = repository.findById(produtoDTO.getId());
+		if (produtoOptional.isPresent()) {
+			Produto produto = produtoOptional.get();
+			produto.setNome(produtoDTO.getNome());
+			produto.setDescricao(produtoDTO.getDescricao());
+			produto.setPreco(produtoDTO.getPreco());
+			produto.setSituacao(produtoDTO.getSituacao());
+			produto.setCategoria(produtoDTO.getCategoria());
+			return repository.save(produto);
+		} else {
+			throw new EntityNotFoundException("Produto não encontrada com ID: " + produtoDTO.getId());
+		}
 	}
 
 }
