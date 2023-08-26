a78a5de3d53b6a84e380d6b2501a342537365910
JoelJonas <joel.deoliveira@salsatechonology.com>
2023-08-26 09:22:23 -0300

    First Commit

---
+++ src/main/java/br/com/bernhoeft/BernhoeftApplication.java
@@ -0,0 +1,13 @@
+package br.com.bernhoeft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BernhoeftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BernhoeftApplication.class, args);
+	}
+
+}
